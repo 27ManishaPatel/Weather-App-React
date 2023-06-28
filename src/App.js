1cc94ef7940591d53394890e1a30cd0c691effdd
27ManishaPatel <manisha270989@gmail.com>
2023-06-28 13:40:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      setLat(pos.coords.latitude);
+      setLong(pos.coords.longitude);
+    });
+    console.log('latitude is:' ,lat)
+    console.log('longitude is:' ,long)
+  }, [lat, long]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
